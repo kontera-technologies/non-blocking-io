6f42e93203306431143324d4be7f3667b442a2be
Eyal Shalev <eyalsh@gmail.com>
2020-03-10 16:51:08 +0200

    .

--- example_test.go
+++ example_test.go
@@ -2,8 +2,8 @@ package non_blocking_io_test
 
 import (
 	"fmt"
-	"log"
 	"os/exec"
+	"strings"
 	"time"
 
 	nbio "github.com/kontera-technologies/non-blocking-io"
@@ -13,7 +13,7 @@ func ExampleNonBlockingStdout() {
 	var err error
 	rw, err := nbio.NewFifo()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	// testdata/foo.sh will output "foo" every 100 milliseconds.
@@ -21,7 +21,7 @@ func ExampleNonBlockingStdout() {
 	cmd.Stdout = rw
 	err = cmd.Start()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer cmd.Process.Kill()
 
@@ -32,15 +32,29 @@ func ExampleNonBlockingStdout() {
 	buf := make([]byte, 100)
 	n, err := rw.Read(buf)
 
-	log.Printf("Took %s to read %d bytes", time.Now().Sub(start), n)
-	log.Printf("Error - %v", err)
-	fmt.Println(string(buf[:n]))
+	if err != nil {
+		panic(err)
+	}
+
+	if time.Now().Sub(start).Microseconds() > 100 {
+		panic(fmt.Sprintf("Took more than 100 microseconds to read %d bytes", n))
+	}
+
+	fmt.Printf("Took less than 100 microseconds to read %d bytes: \"%s\"\n", n, strings.ReplaceAll(string(buf[:n]), "\n", "\\n"))
 
 	// Second read will fail because no data is available.
 	start = time.Now()
 	buf = make([]byte, 100)
 	n, err = rw.Read(buf)
 
-	log.Printf("Took %s to read %d bytes", time.Now().Sub(start), n)
-	log.Printf("Expected Error - %v", err)
+	if time.Now().Sub(start).Microseconds() > 100 {
+		panic(fmt.Sprintf("Took more than 100 microseconds to read %d bytes", n))
+	}
+
+	fmt.Printf("Took less than 100 microseconds to read %d bytes\n", n)
+	fmt.Printf("Expected timeout error - %v\n", err)
+
+	// Output: Took less than 100 microseconds to read 4 bytes: "foo\n"
+	// Took less than 100 microseconds to read 0 bytes
+	// Expected timeout error - resource temporarily unavailable
 }
